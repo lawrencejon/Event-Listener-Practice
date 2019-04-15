3a976d662c2eb13f8c1a23ff733cc515d54534ad
Jonathan Lawrence <lawrencejon97@gmail.com>
2019-04-15 12:59:02 -0400

    declare event function outside events

--- events.js
+++ events.js
@@ -2,17 +2,20 @@ var h1 = document.querySelector("h1");
 var h2 = document.querySelector("h2");
 var button = document.querySelector("button");
 var lis = document.querySelectorAll("li");
-h1.addEventListener("click", () => {
-    document.querySelector("body").style.background = "#a9a9a9";
-});
-h2.addEventListener("click", () => {
-    h2.style.color = "blue";
-});
-button.addEventListener("click", () => {
-    alert("I TOLD YOU DON'T CLICK THE BUTTON!!");
-});
+h1.addEventListener("click", changeBackground);
+h2.addEventListener("click", changeFontColor);
+button.addEventListener("click", buttonAlert);
 for (var i = 0; i < lis.length; i++) {
     lis[i].addEventListener("click", function() {
         this.style.color = "#dd0";
     });
+}
+function changeBackground() {
+    document.querySelector("body").style.background = "#a9a9a9";
+}
+function changeFontColor() {
+    h2.style.color = "#00f";
+}
+function buttonAlert() {
+    alert("I TOLD YOU DON'T CLICK THE BUTTON!!");
 }
